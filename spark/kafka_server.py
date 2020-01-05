0ef59311e0c98f961e2c753769cd2f723f43af66
scott.johnson <scott.johnson@workday.com>
2020-01-05 14:55:12 -0800

    Implemented basic producer and consumer

--- spark/kafka_server.py
+++ spark/kafka_server.py
@@ -1,25 +1,23 @@
 import producer_server
 
+BOOTSTRAP_SERVER = "PLAINTEXT://localhost:18101"
+TOPIC_NAME = "com.udacity.streaming.sfpd"
+INPUT_FILE = "./police-department-calls-for-service.json"
 
 def run_kafka_server():
-	# TODO get the json file path
-    input_file = ""
 
-    # TODO fill in blanks
     producer = producer_server.ProducerServer(
-        input_file=input_file,
-        topic="",
-        bootstrap_servers="",
-        client_id=""
+        input_file=INPUT_FILE,
+        topic=TOPIC_NAME,
+        bootstrap_servers=BOOTSTRAP_SERVER,
+        client_id="sfpd.producer"
     )
 
     return producer
 
-
 def feed():
     producer = run_kafka_server()
     producer.generate_data()
-
 
 if __name__ == "__main__":
     feed()
--- spark/producer_server.py
+++ spark/producer_server.py
@@ -1,25 +1,47 @@
-from kafka import KafkaProducer
+#from kafka import KafkaProducer, KafkaConsumer
+
+from confluent_kafka import Producer, Consumer
+from confluent_kafka.admin import AdminClient, NewTopic
+
 import json
 import time
 
 
-class ProducerServer(KafkaProducer):
+class ProducerServer(Producer):
 
-    def __init__(self, input_file, topic, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, input_file, topic, bootstrap_servers, client_id):
+        conf = {'bootstrap.servers': bootstrap_servers}
+        super().__init__(**conf)
         self.input_file = input_file
         self.topic = topic
+        self.bootstrap_servers = bootstrap_servers
+        self.client_id = client_id
 
-    #TODO we're generating a dummy data
     def generate_data(self):
-        with open(self.input_file) as f:
-            for line in f:
-                message = self.dict_to_binary(line)
-                # TODO send the correct data
-                self.send()
+        self.create_topic()
+        with open(self.input_file) as json_file:
+            data = json.load(json_file)
+            for d in data:
+                print(f"Producing: {d}")
+                self.produce(self.topic, self.dict_to_binary(d))
+                self.flush()
                 time.sleep(1)
 
-    # TODO fill this in to return the json dictionary to binary
     def dict_to_binary(self, json_dict):
-        return 
-        
+        return json.dumps(json_dict).encode('utf-8')
+
+    def create_topic(self):
+        """Creates the topic with the given topic name"""
+        conf = {'bootstrap.servers': self.bootstrap_servers, 'group.id': 'listTopics', 'session.timeout.ms': 6000,
+                'auto.offset.reset': 'latest'}
+        consumer = Consumer(**conf)
+        topics = consumer.list_topics().topics.keys()
+        if (self.topic not in topics):
+            client = AdminClient({"bootstrap.servers": self.bootstrap_servers})
+            futures = client.create_topics([NewTopic(topic=self.topic, num_partitions=1, replication_factor=1)])
+
+            for _, future in futures.items():
+                try:
+                    future.result()
+                except Exception as e:
+                    pass
